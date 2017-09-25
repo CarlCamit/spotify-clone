f656fc209bd6bd2cd84439574c21848a07a2d8c0
Carl Camit <carlcamit0@gmail.com>
2017-09-25 14:47:45 +1000

    added validation to artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,7 @@
 class Artist < ApplicationRecord
+    # 'name' is required
+    # 'biography' has a max of 8000 characters
+
+    validates :name, presence: true 
+    validates :biography, length: { maximum: 8000 }
 end
